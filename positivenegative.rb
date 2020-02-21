cba372e9122a8bdb3df7867d811c456577af87db
carden <carden.ruby@gmail.com>
2020-02-21 23:12:09 +0300

    modification

--- positivenegative.rb
+++ positivenegative.rb
@@ -1,2 +1,3 @@
+# The user enters a number, the program determines a positive or negative number
 puts 'Enter the number (-. +)'
 puts gets.chomp.to_i.positive? ? 'Number is positive' : 'Number is negative'
